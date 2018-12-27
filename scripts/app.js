8c35e210e995a9c67dc7d035c06bf74078af6000
sankhumpha <sankhumpha84@gmail.com>
2018-12-27 13:32:07 +0700

    remove service worker script

--- scripts/app.js
+++ scripts/app.js
@@ -186,13 +186,5 @@
 
   }
   init();
-  // Add feature check for Service Workers here
-
-  /*if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }*/
-
-
+  
 })();
