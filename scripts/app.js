3e62e097fa96a02d35f601969c552fb040a2ef68
sankhumpha <sankhumpha84@gmail.com>
2018-12-27 11:52:43 +0700

    event handle

--- scripts/app.js
+++ scripts/app.js
@@ -20,7 +20,14 @@
     levelComplete: document.querySelector('.level-complete'),
     currentLevel: document.querySelector('.current-level'),
     start: document.querySelector('.start'),
-    refresh: document.querySelector('.button-refresh')
+    refresh: document.querySelector('.button-refresh'),
+    level1: document.querySelector('.level1'),
+    level2: document.querySelector('.level2'),
+    level3: document.querySelector('.level3'),
+    level4: document.querySelector('.level4'),
+    level5: document.querySelector('.level5'),
+    level6: document.querySelector('.level6'),
+
   }
 
   function shuffle(array) {
@@ -99,19 +106,45 @@
         els.currentLevel.textContent = currentLevel + 1;
         setCurrentLevel(currentLevel + 1);
         els.correct.classList.toggle('dialog-container--visible');
+        level(currentLevel);
       }
     } else {
+      var currentLevel = getCurrentLevel();
       els.currentGuess.parentElement.classList.remove('correct');
       els.currentGuess.parentElement.classList.add('wrong');
       els.incorrect.classList.toggle('dialog-container--visible');
       els.correctSequence.textContent = expected.join('');
+      level(currentLevel);
+    }
+  }
+
+  function level(level) {
+    els.level1.classList.add('hide');
+    els.level1.classList.add('hide');
+    els.level1.classList.add('hide');
+    els.level1.classList.add('hide');
+    els.level1.classList.add('hide');
+    els.level1.classList.add('hide');
+
+    if (level == 1 || level < 3) {
+      els.level1.classList.remove('hide');
+    } else if(level >= 3 || level < 5) {
+      els.level2.classList.remove('hide');
+    } else if(level >= 5 || level < 6) {
+      els.level3.classList.remove('hide');
+    } else if(level >=6 || level < 7) {
+      els.level4.classList.remove('hide');
+    } else if(level >= 7 || level < 8) {
+      els.level5.classList.remove('hide');
+    } else if(level >=8){
+      els.level6.classList.remove('hide');
     }
   }
 
 
   function init() {
     els.currentLevel.textContent = getCurrentLevel();
-
+    setCurrentLevel(1);
     possibleEmoji.forEach(function(e) {
       var li = document.createElement('li');
       var btn = document.createElement('button');
