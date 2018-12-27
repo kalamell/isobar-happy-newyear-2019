b18c2008ac10296ec59289d4d96dfb1cceb7b45d
sankhumpha <sankhumpha84@gmail.com>
2018-12-27 13:20:22 +0700

    remove meta

--- scripts/app.js
+++ scripts/app.js
@@ -186,10 +186,11 @@
   init();
   // Add feature check for Service Workers here
 
-  if('serviceWorker' in navigator) {
+  /*if('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
-  }
+  }*/
+  
 
 })();
